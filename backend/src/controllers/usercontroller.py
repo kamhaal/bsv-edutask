a0ba2bae63a47c99133a52c7a42484893e024496
Abdelrman <absn23@studenr.bth.se>
2025-05-15 17:23:22 +0200

    fix the bug in get-user-bu-mail

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -32,9 +32,11 @@ class UserController(Controller):
             users = self.dao.find({'email': email})
             if len(users) == 1:
                 return users[0]
-            else:
-                print(f'Error: more than one user found with mail {email}')
-                return users[0]
+            if len(users) == 0:
+                return None
+            
+            print(f'Error: more than one user found with mail {email}')
+            return users[0]
         except Exception as e:
             raise
 
